c8600d81f10ef1aaee323b099a63af074344a47c
silvermiguel96 <silvermiguel96@gmail.com>
2021-06-15 18:46:38 -0500

    init proyecto

---
+++ server.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const host = process.env.HOST || "127.0.0.1";
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://${host}:${port}`);
+});
